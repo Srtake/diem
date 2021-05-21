8519d0b0fd8473b520437b538a787e7c6aa6b909
unknown <1700132276@qq.com>
2021-05-21 17:44:45 +0800

    fix bugs

--- crypto/crypto/src/pqc_kem.rs
+++ crypto/crypto/src/pqc_kem.rs
@@ -9,6 +9,7 @@ use diem_crypto_derive::{DeserializeKey, SerializeKey, SilentDebug, SilentDispla
 use rand::{CryptoRng, RngCore};
 use std::convert::{TryFrom, TryInto};
 use itertools::Itertools;
+use thiserror::Error;
 
 pub use oqs;
 
@@ -153,10 +154,7 @@ impl PrivateKey {
 
     /// Decapsulate provided raw ciphertext to get the shared secret
     pub fn decapsulate_from_raw(&self, ct: &[u8]) -> oqs::kem::SharedSecret {
-        let ct = match self.KEM.kem.ciphertext_from_bytes(ct) {
-            Some(ct) => ct,
-            None => None
-        };
+        let ct = self.KEM.kem.ciphertext_from_bytes(ct).ok_or(Err(PQCKemError::CiphertextLengthNotCorrect));
         self.decapsulate(&ct.to_owned().clone())
     }
 }
