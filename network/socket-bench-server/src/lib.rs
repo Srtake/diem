bdd3ec438b121c3f913977c8bf5f3c5c5417670d
unknown <1700132276@qq.com>
2021-06-29 18:03:18 +0800

    Modify println info

--- network/socket-bench-server/src/lib.rs
+++ network/socket-bench-server/src/lib.rs
@@ -82,7 +82,8 @@ impl Args {
 /// Build a MemorySocket + Noise transport (No post-quantum support)
 pub fn build_memsocket_noise_transport(remote_public_key: x25519::PublicKey) -> impl Transport<Output = stream::NoiseStream<MemorySocket>> {
     MemoryTransport::default().and_then(move |socket, addr, origin| async move {
-        let mut rng: StdRng = SeedableRng::from_seed(TEST_SEED_2);
+        println!("socket = {:?}, addr = {:?}, origin = {:?}", socket, addr, origin);
+        let mut rng: StdRng = SeedableRng::from_seed(TEST_SEED);
         let private = x25519::PrivateKey::generate(&mut rng);
         let public = private.public_key();
         let peer_id = diem_types::account_address::from_identity_public_key(public);
@@ -103,7 +104,7 @@ pub fn build_memsocket_noise_transport(remote_public_key: x25519::PublicKey) -> 
 /// Build a Tcp + Noise transport
 pub fn build_tcp_noise_transport(remote_public_key: x25519::PublicKey) -> impl Transport<Output = stream::NoiseStream<TcpSocket>> {
     TcpTransport::default().and_then(move |socket, addr, origin| async move {
-        let mut rng: StdRng = SeedableRng::from_seed(TEST_SEED_2);
+        let mut rng: StdRng = SeedableRng::from_seed(TEST_SEED);
         let private = x25519::PrivateKey::generate(&mut rng);
         let public = private.public_key();
         let peer_id = diem_types::account_address::from_identity_public_key(public);
